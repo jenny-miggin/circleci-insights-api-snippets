dfc5fe4eb6ccad35831c5484276ef0ce53a311f7
Jenny Miggin <jenny@circleci.com>
2022-02-08 12:26:11 +0000

    add credits script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # insights-training
 Helpful Scripts for CircleCI Insights API
+
+## Getting Started
+This repo contains sample scripts for pulling and using metrics gathered from CircleCI's Insights API
+
+### API Token
+To generate a token to use in conjunction with the API, please see the docs here: [instructions](https://circleci.com/docs/2.0/managing-api-tokens/#creating-a-personal-api-token)
---
+++ credits-per-project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+### parameters
+api_token=$CIRCLE_API_TOKEN
+org=""
+repos=('repo_1' 'repo_2')
+datetoday=$(date +"%F")
+dayrange="60" # number of days you want to report on
+
+### report generation
+echo "Starting to generate report..."
+echo 'The current date is:' >> credit_report.csv
+echo "$datetoday" >> credit_report.csv
+echo '----------------------------------------------------------' >> credit_report.csv
+
+for i in "${repos[@]}"
+do
+      echo "Populating report for $i"
+      response=$(curl -s -u ${api_token}: https://circleci.com/api/v2/insights/gh/$org/$i/workflows?reporting-window=last-$dayrange-days)
+      echo "$response" >> temp.csv
+      echo "Total credits used during the last 60 days for ${i} is:" >> credit_report.csv
+      response3=$(cat temp.csv | sed 's/.*"total_credits_used" : //' | cut -d " " -f 1 | sed -e '/^$/d' | sed '/^{/d' | sed '/^}/d' | sed 's/\(. *\),/\1/' | awk '{s+=$1} END {print s}')
+      echo "$response3" >> credit_report.csv
+      echo '----------------------------------------------------------' >> credit_report.csv
+      rm temp.csv
+
+done
+echo "Finished report!"
